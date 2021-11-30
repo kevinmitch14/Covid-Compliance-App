b926d4fdc52f515dc79d47acfea4d6014408b79b
Kevin Mitchell <kevinmitch14@gmail.com>
2021-11-30 12:28:43 +0000

    Recoil implementation and redux removal

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { Provider } from 'react-redux'
-import store from './store'
+import { RecoilRoot } from 'recoil';
 
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  // <React.StrictMode>
-  // </React.StrictMode>,
-  document.getElementById('root')
+    <RecoilRoot>
+        <App />
+    </RecoilRoot>
+    ,
+    // <React.StrictMode>
+    // </React.StrictMode>,
+    document.getElementById('root')
 );
 
 serviceWorker.unregister();
